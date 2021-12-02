8dfdd2647e07d9b68fe76d041f496117377a2575
Christopher Hunter <hunter.christopher@icloud.com>
2021-12-02 10:58:00 -0800

    feature: run button runs built wasm binary

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,9 +8,11 @@ import (
 	"io"
 	"io/fs"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -22,6 +24,7 @@ func main() {
 
 	mux.Handle("/go/version", handleVersion())
 	mux.Handle("/go/env", handleEnv())
+	mux.Handle("/go/run", handleRun())
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
@@ -45,6 +48,120 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func handleRun() http.HandlerFunc {
+	goExecPath, lookUpErr := exec.LookPath("go")
+	if lookUpErr != nil {
+		panic(lookUpErr)
+	}
+
+	env := mergeEnv(os.Environ(), goEnvOverride()...)
+
+	return func(res http.ResponseWriter, req *http.Request) {
+		ctx, cancel := context.WithTimeout(req.Context(), time.Second*5)
+		defer cancel()
+
+		tmp, err := os.MkdirTemp("", "")
+		if err != nil {
+			http.Error(res, "failed to create temporary directory", http.StatusInternalServerError)
+			return
+		}
+		defer func() {
+			_ = os.RemoveAll(tmp)
+		}()
+
+		err = createMainFile(tmp, req.Body)
+		if err != nil {
+			http.Error(res, "failed to write main.go", http.StatusInternalServerError)
+			return
+		}
+		err = createModFile(tmp)
+		if err != nil {
+			http.Error(res, "failed to create go.mod", http.StatusInternalServerError)
+			return
+		}
+
+		const output = "main.wasm"
+		cmd := exec.CommandContext(ctx, goExecPath,
+			"build",
+			"-o", output,
+		)
+		var stdout, stderr bytes.Buffer
+		cmd.Stdout = &stdout
+		cmd.Stderr = &stderr
+		cmd.Env = env
+		cmd.Dir = tmp
+		err = cmd.Run()
+		if err != nil {
+			http.Error(res, stderr.String(), http.StatusInternalServerError)
+			return
+		}
+
+		b, err := os.Open(filepath.Join(tmp, output))
+		if err != nil {
+			http.Error(res, "failed to open build file", http.StatusInternalServerError)
+			return
+		}
+		defer func() {
+			_ = b.Close()
+		}()
+		mw := multipart.NewWriter(res)
+		defer func() {
+			_ = mw.Close()
+		}()
+		res.Header().Set("Content-Type", mw.FormDataContentType())
+		res.WriteHeader(http.StatusOK)
+
+		err = mw.WriteField("stdout", stdout.String())
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		err = mw.WriteField("stderr", stderr.String())
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		wasm, err := mw.CreateFormFile("output", "main.wasm")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		_, _ = io.Copy(wasm, b)
+	}
+}
+
+func createMainFile(dir string, rc io.ReadCloser) error {
+	fp := filepath.Join(dir, "main.go")
+	f, err := os.Create(fp)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+	_, err = io.Copy(f, rc)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func createModFile(dir string) error {
+	fp := filepath.Join(dir, "go.mod")
+	f, err := os.Create(fp)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+	_, err = f.Write([]byte("module playground\n"))
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func handlePage(res http.ResponseWriter, req *http.Request) {
--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -5,8 +5,13 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
+	"mime"
+	"mime/multipart"
 	"net/http"
+	"strings"
 	"sync"
+	"syscall/js"
 
 	"github.com/crhntr/window"
 )
@@ -19,6 +24,99 @@ func main() {
 	}
 
 	doAll(fetchGoVersion, fetchGoEnv)
+
+	window.Document.QuerySelector("button#run").AddEventListenerFunc("click", func(event window.Event) {
+		go run()
+	})
+
+	select {}
+}
+
+func run() {
+	code := window.Document.QuerySelector("textarea#code").Get("value").String()
+
+	req, err := http.NewRequest(http.MethodPost, "/go/run", strings.NewReader(code))
+	if err != nil {
+		fmt.Println("failed to create request", err)
+		return
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("failed to do request", err)
+		return
+	}
+	defer func() {
+		_ = res.Body.Close()
+	}()
+
+	if res.StatusCode != http.StatusOK {
+		buf, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("failed to read response", err)
+			return
+		}
+		window.Document.QuerySelector("ul#errors").Append(window.Document.NewElement("%s", string(buf)))
+		return
+	}
+
+	_, params, err := mime.ParseMediaType(res.Header.Get("Content-Type"))
+	if err != nil {
+		fmt.Println("failed to parse response mimetype", err)
+		return
+	}
+	r := multipart.NewReader(res.Body, params["boundary"])
+
+	for {
+		part, err := r.NextRawPart()
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("failed to read response part", err)
+				return
+			}
+			break
+		}
+
+		switch part.FormName() {
+		case "stdout":
+			buf, err := io.ReadAll(part)
+			if err != nil {
+				fmt.Println("failed to read response stdout", err)
+				return
+			}
+			window.Document.QuerySelector("pre#stdout").SetInnerText(string(buf))
+		case "stderr":
+			buf, err := io.ReadAll(part)
+			if err != nil {
+				fmt.Println("failed to read response stdout", err)
+				return
+			}
+			window.Document.QuerySelector("pre#stderr").SetInnerText(string(buf))
+		case "output":
+			buf, err := ioutil.ReadAll(part)
+			if err != nil {
+				fmt.Println("failed to read response output", err)
+				return
+			}
+			g := js.Global()
+			uint8Array := g.Get("Uint8Array").New(len(buf))
+			_ = js.CopyBytesToJS(uint8Array, buf)
+
+			done := make(chan struct{})
+			wasmExec := g.Get("Go").New()
+			success := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+				defer close(done)
+				result := args[0]
+				wasmExec.Call("run", result.Get("instance"))
+				return nil
+			})
+
+			g.Get("WebAssembly").Call(
+				"instantiate", uint8Array.Get("buffer"), wasmExec.Get("importObject"),
+			).Call("then", success)
+
+			<-done
+		}
+	}
 }
 
 func fetchGoVersion() {
