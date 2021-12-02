b8ba706781a70155f430499cd4642e3a60a15c54
Christopher Hunter <hunter.christopher@icloud.com>
2021-12-02 01:11:44 -0800

    feature: add some Go command debug details
    this is just to write some handlers that interact
    with the Go command

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"embed"
+	"fmt"
 	"html/template"
+	"io"
 	"io/fs"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.FS(assetsFS))))
 	mux.Handle("/", http.HandlerFunc(handlePage))
+
+	mux.Handle("/go/version", handleVersion())
+	mux.Handle("/go/env", handleEnv())
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
@@ -54,4 +65,78 @@ func handlePage(res http.ResponseWriter, req *http.Request) {
 	if err := templates.Execute(res, struct{}{}); err != nil {
 		return
 	}
+}
+
+func handleVersion() http.HandlerFunc {
+	goExecPath, lookUpErr := exec.LookPath("go")
+	if lookUpErr != nil {
+		panic(lookUpErr)
+	}
+
+	env := mergeEnv(os.Environ(), goEnvOverride()...)
+
+	return func(res http.ResponseWriter, req *http.Request) {
+		ctx, cancel := context.WithTimeout(req.Context(), time.Second*2)
+		defer cancel()
+
+		cmd := exec.CommandContext(ctx, goExecPath, "version")
+		var buf bytes.Buffer
+		cmd.Stdout = &buf
+		cmd.Stderr = &buf
+		cmd.Env = env
+		err := cmd.Run()
+		if err != nil {
+			http.Error(res, buf.String(), http.StatusInternalServerError)
+			return
+		}
+		res.WriteHeader(http.StatusOK)
+		_, _ = io.Copy(res, &buf)
+	}
+}
+
+func handleEnv() http.HandlerFunc {
+	goExecPath, lookUpErr := exec.LookPath("go")
+	if lookUpErr != nil {
+		panic(lookUpErr)
+	}
+
+	env := mergeEnv(os.Environ(), goEnvOverride()...)
+	fmt.Println(env)
+
+	return func(res http.ResponseWriter, req *http.Request) {
+		ctx, cancel := context.WithTimeout(req.Context(), time.Second*2)
+		defer cancel()
+
+		cmd := exec.CommandContext(ctx, goExecPath, "env")
+		var buf bytes.Buffer
+		cmd.Stdout = &buf
+		cmd.Stderr = &buf
+		cmd.Env = env
+		err := cmd.Run()
+		if err != nil {
+			http.Error(res, buf.String(), http.StatusInternalServerError)
+			return
+		}
+		res.WriteHeader(http.StatusOK)
+		_, _ = io.Copy(res, &buf)
+	}
+}
+
+func goEnvOverride() []string { return []string{"GOOS=js", "GOARCH=wasm"} }
+
+func mergeEnv(env []string, additional ...string) []string {
+	l := len(env) + len(env)
+	m := make(map[string]string, l)
+	keys := make([]string, 0, l)
+	for _, v := range append(additional, env...) {
+		x := strings.Index(v, "=")
+		k := v[:x]
+		keys = append(keys, k)
+		m[k] = v[x+1:]
+	}
+	result := make([]string, 0, l)
+	for _, k := range keys {
+		result = append(result, k+"="+m[k])
+	}
+	return result
 }
--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -1,7 +1,55 @@
-//go:build wasm && js
+//go:build js && wasm
 
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/crhntr/window"
+)
+
 func main() {
-	println("Hello, world!")
+	_, err := window.LoadTemplates(nil, "template")
+	if err != nil {
+		fmt.Println("failed to load template", err)
+		return
+	}
+	fetchGoVersion()
+	fetchGoEnv()
+}
+
+func fetchGoVersion() {
+	res, err := http.Get("/go/version")
+	if err != nil {
+		fmt.Println("failed to fetch version", err)
+		return
+	}
+	defer func() {
+		_ = res.Body.Close()
+	}()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("failed to read version", err)
+		return
+	}
+	window.Document.QuerySelector("details#go-version pre").SetInnerText(string(body))
+}
+
+func fetchGoEnv() {
+	res, err := http.Get("/go/env")
+	if err != nil {
+		fmt.Println("failed to fetch version", err)
+		return
+	}
+	defer func() {
+		_ = res.Body.Close()
+	}()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("failed to read version", err)
+		return
+	}
+	window.Document.QuerySelector("details#go-env pre").SetInnerText(string(body))
 }
