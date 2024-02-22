8d857a81c9f1260a23941c13a4e25a1259970e18
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2024-02-21 22:15:43 -0800

    remove deprecated method from example

--- cmd/server/assets/examples/counting-up/main.go
+++ cmd/server/assets/examples/counting-up/main.go
@@ -8,15 +8,14 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
-	max := rand.Intn(8) + 2
-	fmt.Printf("max: %d\n", max)
+	n := rand.Intn(8) + 2
+	fmt.Printf("n: %d\n", n)
 
 	body := js.Global().Get("document").Get("body")
 	body.Set("innerHTML", `<ul></ul>`)
 
 	ul := body.Call("querySelector", "ul")
-	for i := 0; i < max; i++ {
+	for i := 0; i < n; i++ {
 		ul.Call("insertAdjacentHTML", "beforeEnd", fmt.Sprintf("<li>%d</li>", i))
 		time.Sleep(time.Second / 2)
 	}
